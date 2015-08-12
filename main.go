adcd3a707209fecfc59e7b07f844d378a1774153
Blake Gentry <blakesgentry@gmail.com>
2015-08-12 10:45:43 -0700

    basic fetching + resizing of images

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/nfnt/resize"
 )
 
 func main() {
@@ -22,5 +28,57 @@ func handleResize(w http.ResponseWriter, req *http.Request, params httprouter.Pa
 		http.Error(w, "invalid source URL", 400)
 		return
 	}
-	w.Write([]byte("ok\n"))
+
+	sizeParts := strings.Split(params.ByName("size"), "x")
+	if len(sizeParts) != 2 {
+		http.Error(w, "invalid width requested", 400)
+		return
+	}
+	width, err := strconv.ParseUint(sizeParts[0], 10, 64)
+	if err != nil {
+		http.Error(w, "invalid width requested", 400)
+		return
+	}
+	height, err := strconv.ParseUint(sizeParts[1], 10, 64)
+	if err != nil {
+		http.Error(w, "invalid height requested", 400)
+		return
+	}
+
+	resp, err := http.Get(sourceURL.String())
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		copyHeader(w.Header(), resp.Header)
+		io.Copy(w, resp.Body)
+		return
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	if !strings.HasPrefix(contentType, "image/") {
+		http.Error(w, fmt.Sprintf("invalid content type %q", contentType), 500)
+		return
+	}
+
+	img, _, err := image.Decode(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	imgResized := resize.Resize(uint(width), uint(height), img, resize.Bicubic)
+	w.Header().Set("Content-Type", "image/jpeg")
+	// TODO(bgentry) set other headers
+	jpeg.Encode(w, imgResized, nil)
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
 }
